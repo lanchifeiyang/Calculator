4ce401b3007d14114dbfe6c238a14a99931a8140
chenzs <qiuyefengxi@163.com>
2018-05-24 17:39:22 +0800

    test

--- app/src/main/java/com/project/chenzs/calculatorapplication/CalculatorActivity.java
+++ app/src/main/java/com/project/chenzs/calculatorapplication/CalculatorActivity.java
@@ -9,8 +9,5 @@ public class CalculatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
-        int i=0;
-        int b=0;
-        int c=0;
     }
 }
